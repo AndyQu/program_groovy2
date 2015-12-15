718f399a1cdada5e15bb0d1f292ab9f45dcaccc0
quguocheng <quguocheng@meituan.com>
2015-12-15 11:22:02 +0800

    使用ExpandoMetaClass注入 函数、static函数、property、constructor，替换constructor（AOP）

--- InjectionAndSynthesisWithMOP/UseExpandoMetaClass.groovy
+++ InjectionAndSynthesisWithMOP/UseExpandoMetaClass.groovy
@@ -1,35 +1,51 @@
+/*
+使用metaClass可以：
+    注入新函数
+    注入新property
+    注入static函数
+    注入新constructor
+    替换constructor
+
+*/
+
+//注入新函数
 Integer.metaClass.getDaysFromNow={
-	_->
-		Calendar today = Calendar.instance
-		today.add(Calendar.DAY_OF_MONTH, delegate)
-		today.time
+    _->
+        Calendar today = Calendar.instance
+        today.add(Calendar.DAY_OF_MONTH, delegate)
+        today.time
 }
 println 5.getDaysFromNow()
 
+//注入新property
+Integer.metaClass.getDaysFromNow={
+    ->
+        Calendar today = Calendar.instance
+        today.add(Calendar.DAY_OF_MONTH, delegate)
+        today.time
+}
+println 5.daysFromNow
 
+//注入static函数
 Integer.metaClass.static.isEven={
-	it->it%2==0
+    it->it%2==0
 }
-//Integer.metaClass.methods.each{println it}
-try{
-	println Integer.isEven(3)
-	println Integer.isEven(2)
-}catch(Exception e){
-	println e
-}
+println Integer.isEven(3)
+println Integer.isEven(2)
 
-
+//注入新的constructor
 Integer.metaClass.constructor << {
-	Calendar cal->
-		new Integer(cal.get(Calendar.DAY_OF_YEAR))
+    Calendar cal->
+        new Integer(cal.get(Calendar.DAY_OF_YEAR))
 }
 println new Integer(Calendar.instance)
 
+//替换constructor
 Integer.metaClass.constructor={
-	Integer val->
-		println "intercepted constructor"
-		def method=Integer.class.getConstructor(Integer.TYPE)
-		method.newInstance(val)
+    Integer val->
+        println "intercepted constructor"
+        def method=Integer.class.getConstructor(Integer.TYPE)
+        method.newInstance(val)
 }
 println new Integer(56)
 println new Integer(Calendar.instance)
