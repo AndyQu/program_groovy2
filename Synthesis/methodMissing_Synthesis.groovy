7881d14e7dc4c7171c042b58e147b043b2ca0d90
quguocheng <quguocheng@meituan.com>
2015-12-31 17:08:40 +0800

    使用this.getMetaClass()而不是this.metaClass注入方法

--- Synthesis/methodMissing_Synthesis.groovy
+++ Synthesis/methodMissing_Synthesis.groovy
@@ -1,25 +1,25 @@
 class Person{
+    def pname
     def work(){
         println "work()"
     }
     def sports=['basketball','football','voleyball']
     def methodMissing(String name, args){
         if(name in sports){
-            Person instance=this
-            println "inject method ${name} into Person"
-            Person.metaClass."$name"={
+            //Person instance=this
+            println "inject method ${name} into Person ${pname}"
+            this.getMetaClass()."$name"={
                 ->
-                println "${name}..."
+                println "${pname} is ${name}..."
             }
-            //instance.metaClass.invokeMethod(name, args)
+            this.getMetaClass().getMetaMethod(name, args).invoke(this,args)            
         }else{
             println "no such method:${name}() in Person class"
         }
     }
 }
-def jack=new Person()
+def jack=new Person(pname:"jack")
 jack.football()
 jack.football()
-jack.football()
-def rose=new Person()
+def rose=new Person(pname:"rose")
 rose.football()
